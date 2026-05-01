8cca09cdbd59369da13db7b076e2af636eb1039f
Mahfuz Shakib <mahfuzshakib301@gmail.com>
2026-05-01 19:54:30 +0600

    swap two variable without temp - by js

--- 1.Basic-Logic-Math/3.swap-two-variable.js
+++ 1.Basic-Logic-Math/3.swap-two-variable.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync(0,'utf-8').trim();
 const arr = input.split(' ').map(Number).slice(0,2);
+
 let a = arr[0], b=arr[1];
-console.log(a,b);
+console.log("before swap: ",a,b);
 a+=b;
 b=a-b;
 a-=b;
-console.log(a,b);
+console.log("after swap: ",a,b);
